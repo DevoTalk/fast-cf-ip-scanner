48957c30f6d8c2a4ae908f7b3ad3f90338191f3a
ali h <azatazaz1ta9@gmail.com>
2023-04-24 20:43:46 +0330

    Configure app for connect to db

---
+++ fast cf ip scanner/Constants/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace fast_cf_ip_scanner.Constants
+{
+    public static class Constants
+    {
+        public const string DatabaseFilename = "FastCFIPScanner.db";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            // open the database in read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            // create the database if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            // enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath =>
+            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    }
+}
