0d21924cff341cf6c161871e2b81588673ae682f
ali h <azatazaz1ta9@gmail.com>
2024-06-09 17:45:53 +0330

    add feat for export all ips

--- fast cf ip scanner/ViewModels/ScanPageViewModel.cs
+++ fast cf ip scanner/ViewModels/ScanPageViewModel.cs
@@ -12,16 +12,19 @@ namespace fast_cf_ip_scanner.ViewModels
         ObservableCollection<IPModel> validIPs;
 
         [ObservableProperty]
+        bool exportBtnVisible = false;
+
+        [ObservableProperty]
         bool isBusy = false;
 
         IpOptionModel _ipOption;
-        
+
         readonly IPService _iPServices;
         readonly WorkerService _workerServices;
-        public ScanPageViewModel(IPService iPServices,WorkerService workerService)
+        public ScanPageViewModel(IPService iPServices, WorkerService workerService)
         {
             validIPs = new ObservableCollection<IPModel>();
-            
+
             _iPServices = iPServices;
             _workerServices = workerService;
 
@@ -33,15 +36,16 @@ namespace fast_cf_ip_scanner.ViewModels
         {
             var protocols = new string[] { "Http test", "TCP test", "Terminal Ping test" };
 
-            var selectedProtocol = 
+            var selectedProtocol =
                 await App.Current.MainPage.DisplayActionSheet("which protocol", "Cancel", null, protocols);
-            
-            if(selectedProtocol == "Cancel" || selectedProtocol == null)
+
+            if (selectedProtocol == "Cancel" || selectedProtocol == null)
             {
                 return;
             }
 
             IsBusy = true;
+            ExportBtnVisible = false;
 
             List<IPModel> validIp = new List<IPModel>();
 
@@ -50,7 +54,7 @@ namespace fast_cf_ip_scanner.ViewModels
             var ips = await _iPServices.GetIps();
             if (ips.Length < 1)
             {
-                await App.Current.MainPage.DisplayAlert("Error", $"have a error try again ","OK");
+                await App.Current.MainPage.DisplayAlert("Error", $"have a error try again ", "OK");
             }
             else
             {
@@ -68,8 +72,9 @@ namespace fast_cf_ip_scanner.ViewModels
                 await _iPServices.AddValidIpToDb(validIp);
             }
             IsBusy = false;
+            ExportBtnVisible = true;
         }
-        
+
         [RelayCommand]
         async Task ShowOptionsForSearchIp()
         {
@@ -80,6 +85,13 @@ namespace fast_cf_ip_scanner.ViewModels
         }
 
         [RelayCommand]
+        async Task ExportAllIPsToClipboard()
+        {
+            var ips = string.Join(",", ValidIPs.Select(ip => ip.IP.ToString()));
+            await Clipboard.SetTextAsync(ips);
+            await App.Current.MainPage.DisplayAlert("Copied", $"the ips is copied", "OK");
+        }
+        [RelayCommand]
         async Task ShowSelectedIPOption(IPModel ipModel)
         {
             if (ipModel != null)
@@ -87,7 +99,7 @@ namespace fast_cf_ip_scanner.ViewModels
                 var workers = await _workerServices.GetWorkers();
 
                 string[] Options;
-                if(workers.Count == 0)
+                if (workers.Count == 0)
                 {
                     Options = new string[3];
                     Options[2] = "Please add a worker";
@@ -99,10 +111,10 @@ namespace fast_cf_ip_scanner.ViewModels
                 Options[0] = "Copy";
 
                 Options[1] = "speed test";
-                
+
                 for (int i = 2; i <= workers.Count; i++)
                 {
-                    Options[i] = workers[i-2].Url;
+                    Options[i] = workers[i - 2].Url;
                 }
                 var reslut = await App.Current.MainPage.DisplayActionSheet($"What to Do With {ipModel.IP}", null, null, Options);
 
@@ -114,7 +126,7 @@ namespace fast_cf_ip_scanner.ViewModels
                         await Clipboard.SetTextAsync(ip);
                         await App.Current.MainPage.DisplayAlert("Copied", $"the {ip} is copied", "OK");
                     }
-                    else if(reslut== "speed test")
+                    else if (reslut == "speed test")
                     {
                         IsBusy = true;
                         var downloadSizeForSpeedTestToMB = _ipOption.DownloadSizeForSpeedTest * 1024 * 1024;
@@ -122,7 +134,7 @@ namespace fast_cf_ip_scanner.ViewModels
                         IsBusy = false;
                         await App.Current.MainPage.DisplayAlert("speed", $"{speed} Mb", "ok");
                     }
-                    else if(reslut == "Please add a worker")
+                    else if (reslut == "Please add a worker")
                     {
                         await Shell.Current.GoToAsync(nameof(SettingPage));
                         return;
